f47e626b23610898939d79dabe9d3a570dd66b2b
Kelsey Langlois and Megan Olson <kelsey@>
2017-09-11 10:47:00 -0700

    Add user interface

--- app.rb
+++ app.rb
@@ -8,5 +8,12 @@ get('/') do
 end
 
 get('/output') do
+  length = params["length"].to_i
+  width = params["width"].to_i
+  height = params["height"].to_i
+  weight = params["weight"].to_i
+  shipping = params["shipping"]
+  parcel = Parcel.new(length, width, height, weight)
+  @cost = parcel.cost_to_ship(shipping)
   erb(:output)
 end
